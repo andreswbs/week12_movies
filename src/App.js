864218d1e067691ff1730a73a8c24fc6b57522a7
Andres Järviste <andres.jarviste@wbscodingschool.com>
2022-07-06 22:32:42 +0300

    fetching movies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([])
+  const [searchWord, setSearchWord] = useState('destiny')
+
+  const readMovies = async () => {
+    const response = await fetch(`
+    https://api.themoviedb.org/3/search/movie?api_key=24c64ea903d3b9426c0b72f5af3d2813&language=en-US&query=${searchWord}&page=1&include_adult=false
+    `)
+    const result = await response.json()
+    console.log(result)
+    setMovies(()=> result.results)
+  }
+
+  const moviesList = movies.map((movie, index) => {
+    return (
+    <li key={index}>
+      <h3>{movie.title}</h3>
+      <div>
+        {movie.overview}
+      </div>
+    </li>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Movies</h1>
+        <button onClick={readMovies}>Search</button>
+        <div>
+          <ul>
+            {moviesList}
+          </ul>
+        </div>
+      </div>
   );
 }
 
